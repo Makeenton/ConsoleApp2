64e93346e4e66cfb51b79ef17ed0f0ffb88a0dd9
makeenton <anton.makeyenko@gmail.com>
2021-04-08 16:41:06 +0700

    Новая библиотека

---
+++ ActiveD/ADProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+
+namespace ActiveD
+{
+    public class ADProvider
+    {
+        /// <summary>
+        /// Поиск всех пользователей в АД
+        /// </summary>
+        /// <returns>Список пользователей</returns>
+        public List<ADUserProperties> GetAllUsers()
+        {
+
+            List<ADUserProperties> users = new List<ADUserProperties>();
+
+            DirectoryEntry CurrentDomain = new DirectoryEntry();
+            DirectorySearcher adSearcher = new DirectorySearcher(CurrentDomain);
+            adSearcher.Filter = "(&(objectClass=user)(objectCategory=person))";
+            SearchResultCollection resultCol = adSearcher.FindAll();
+            
+            foreach (SearchResult result in resultCol)
+            {
+                ADUserProperties user = new ADUserProperties();
+
+                user.Login = (String)result.Properties["samaccountname"][0];
+                user.UserName = (String)result.Properties["cn"][0];
+
+                users.Add(user);
+
+            }
+            return users;
+
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.DirectoryServices;
+using ActiveD;
 
 namespace ConsoleApp2
 {
@@ -11,24 +11,12 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            ADProvider ad = new ADProvider();
+            List<ADUserProperties> users = ad.GetAllUsers();
 
-
-            DirectoryEntry CurrentDomain = new DirectoryEntry();
-
-            DirectorySearcher adSearcher = new DirectorySearcher(CurrentDomain);
-
-            adSearcher.Filter = "(&(objectClass=user)(objectCategory=person))";
-        
-            Console.WriteLine("Listing of users in the Active Directory");
-            Console.WriteLine("========================================");
-            SearchResultCollection resultCol = adSearcher.FindAll();
-
-            foreach (SearchResult result in resultCol)
+            foreach (ADUserProperties user in users)
             {
-                string Login = (String)result.Properties["samaccountname"][0];
-                string UserName = (String)result.Properties["cn"][0];
-
-                Console.WriteLine($"{UserName}({Login})" );
+                Console.WriteLine($"{user.UserName}({user.Login})" );
                                 
             }
 

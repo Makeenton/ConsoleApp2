96ab1e18e4c519d0c7296975857b9678310c4fca
makeenton <anton.makeyenko@gmail.com>
2021-04-13 11:38:20 +0700

    Новая функция поиска по логину

---
+++ ADSearcher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActiveD;
+
+
+namespace ADSearcher
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            ADSearch ctx = new ADSearch();
+            string User = ctx.GetUser().ToString();
+            Console.WriteLine($"{User}");
+            Console.ReadKey();
+     
+        }
+    }
+}
---
+++ ActiveD/ADSearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveD
+{
+    public class ADSearch
+    {
+        public string GetUser()
+        {
+            Console.Write("Введите логин: ");
+            string login = Console.ReadLine();
+            PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
+            UserPrincipal user = UserPrincipal.FindByIdentity(ctx, login);
+            
+            
+            if (user != null)
+            {
+                return user.ToString();
+            }
+            else
+                return "Пользователь не найден";
+                        
+
+        }
+    }
+}
